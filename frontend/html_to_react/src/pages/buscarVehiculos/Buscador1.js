9ecd834745b9231aed1100ceff6883fe3d88efe7
Alberto Jimenez A <alberto.aja12@gmail.com>
2024-01-24 23:39:08 +0100

    /busquedaVehiculos

---
+++ frontend/html_to_react/src/pages/buscarVehiculos/Buscador1.js
@@ -0,0 +1,71 @@
+
+import React, { useState, useEffect } from 'react';
+// import SearchBar from './components/SearchBar';
+import SearchResultsCars from '../../components/body/SearchResultsCars';
+import axios from 'axios';
+
+import { useModal } from '../../ModalContext';
+import FilterModal from '../../components/filter-modal';
+
+//  modificar los Query String de las URLs
+// import { useSearchParams } from "react-router-dom";
+
+export default function Buscador1() {
+
+  //  modificar los Query String de las URLs
+  // let [searchParams, setSearchParams] = useSearchParams();
+  // const App = () => {
+    const [results, setResults] = useState([]);
+
+    const searchApi = async (query) => {
+      try {
+        // OK  const response = await axios.get(`https://api.example.com/search?q=${query}`);  // ejecuta GET por cada letra se tecleea. EMpieza a ejecutar a partir de 3 letras tecleadas en el buscador
+       
+        //ok para mostrar 1 tabla. responde 1 array de objetos 
+        // const response = await axios.get("https://jsonplaceholder.typicode.com/users");    //  da 10 users siempre
+        // responde 1 objetos de elementos
+        const response = await axios.get(` https://myfakeapi.com/api/cars/name/${query}`);
+        
+        // opciones de cars:(campo car del API) :  Mitsubishi, Volkswagen, Saturn, Jeep, Chevrolet, Dodge, Isuzu, ...
+
+        //response.setResults="datos aja en js";
+        setResults(response.data);
+        // console.log(/*"resultados  consulta: "+*/ response.data);
+      } catch (error) {
+        console.error('Error al realizar la búsqueda:', error);
+      }
+    };
+
+    //  modificar los Query String de las URLs
+    // desde react r dom 6.0.0 :    https://reactrouter.com/en/main/hooks/use-search-params#usesearchparams
+    // setSearchParams(campos);
+    const filtrarCampos =(campos) => {
+      console.log(campos)    // campos=valor seleccionado en el input type=radio del index de FilterModal.jsx
+      
+    };
+
+    useEffect(() => {
+      // Realizar una búsqueda inicial al cargar la página
+      //searchApi('initialQuery');  // valor de la busqueda inicial
+      searchApi('Mitsubishi');
+    }, []); // El array de dependencias vacío asegura que solo se ejecute al montar el componente
+
+   
+  // };
+
+  const { open, handleOpen, handleClose } = useModal();
+  
+
+  return (
+    <div>
+      <button className="boton__contáctanos" onClick={handleOpen}>Filtrar Resultados</button>
+      <FilterModal isOpen={open} handleClose={handleClose} onFilter={filtrarCampos} />
+
+      <h3>Resultados para esas fechas:</h3>        
+      {/* <SearchBar onSearch={searchApi} /> */}      
+     
+      <SearchResultsCars  results={results} />
+    </div>
+  );
+
+}
