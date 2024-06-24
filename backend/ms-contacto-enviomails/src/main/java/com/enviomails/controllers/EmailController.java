08369523c708913613789c03ef0da9d2555c4bc1
Alberto Jimenez A <alberto.aja12@gmail.com>
2024-06-24 08:07:35 +0200

    activar swagger/openapi

--- backend/ms-contacto-enviomails/src/main/java/com/enviomails/controllers/EmailController.java
+++ backend/ms-contacto-enviomails/src/main/java/com/enviomails/controllers/EmailController.java
@@ -9,17 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.enviomails.dto.EmailyReservaRequest;
 import com.enviomails.services.EmailService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 //@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/efast/v1")
+@Tag(name = "send email", description = "API Documentation for sending HTML template contact form using Google SMTP service in eFast web app")
 public class EmailController {
 
 	@Autowired
     private EmailService emailService;
 
 
+	@Operation(summary = " endpoint to send email", description = " email endpoint. ", tags = {})
     @PostMapping("/emails/enviar")
-    public String sendTestReport(@RequestBody EmailyReservaRequest emailRequest) {
+    public String sendEmail(@RequestBody EmailyReservaRequest emailRequest) {
         return emailService.ServiceMail(emailRequest);
     }
   
