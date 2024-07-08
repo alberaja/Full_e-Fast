f0eae6f83ce6eecd9dd3ef5b4b72d849217a3c3f
Alberto Jimenez A <alberto.aja12@gmail.com>
2024-07-08 08:56:54 +0200

    add Author

--- backend/ms-elastic-eFast/src/main/java/com/elastic/efast/ElasticEFastApplication.java
+++ backend/ms-elastic-eFast/src/main/java/com/elastic/efast/ElasticEFastApplication.java
@@ -3,6 +3,12 @@ package com.elastic.efast;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/***************
+ * @author Alberto Jimenez Arregui
+ * El proyecto ha sido desarrollado como parte del Trabajo de Fin de Estudio, 
+ * en el Grado en Ingeniería Informática de UNIR.
+****************/
+
 @SpringBootApplication
 // If we don’t use @Configuration in the POJO, then we need to add @EnableConfigurationProperties(ConfigProperties.class) in the main Spring application class to bind the properties into the POJO.
 //@EnableConfigurationProperties(ConfigProperties.class)
